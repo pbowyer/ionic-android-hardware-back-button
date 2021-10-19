e5bc11a71fa217cd07a2a9c3fd1608c2ed89ca84
Peter Bowyer <peter@mapledesign.co.uk>
2021-10-19 15:53:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-android-hardware-back-button',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
